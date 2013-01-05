069649664f4d17f9b4f70804adba24f86aa3a066
Freek Post <f.post@student.tudelft.nl>
2013-01-05 19:01:59 +0100

    Added existing unit tests from the jMonkeyEngine project Added jUnit libraries Added jMockit library

---
+++ jme3/test/com/jme3/material/MaterialTest.java
@@ -0,0 +1,226 @@
+package com.jme3.material;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jme3.material.Material;
+
+public class MaterialTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMaterialMaterialDef() {
+		//fail("Not yet implemented");
+		//assert(true);
+		Material material = new Material();
+		material.setInt("test", 5);
+		assertTrue(true);
+	}
+
+	/*@Test
+	public void testMaterialAssetManagerString() {
+		//fail("Not yet implemented");
+		assert(true);
+	}
+
+	@Test
+	public void testMaterial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSortId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContentEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContentHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActiveTechnique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTransparent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTransparent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsReceivesShadows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReceivesShadows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAdditionalRenderState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaterialDef() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTextureParam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParams() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetParam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearParam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTextureParam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTexture() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMatrix4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVector2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVector3() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVector4() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLightListUniforms() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderMultipassLighting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectTechnique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPreload() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRender() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRead() {
+		fail("Not yet implemented");
+	}*/
+
+}
